c538df8310580861b6bad0e0803582a04063c17d
jacek-raczkiewicz <jacek@skycore.com>
2013-08-14 19:32:48 +0200

    Update skycore_sdk.php

--- 1.0/source_code/skycore_sdk.php
+++ 1.0/source_code/skycore_sdk.php
@@ -6,7 +6,7 @@ class Skycore	{
 	
 	public function Skycore($key)	{
 	
-		$this->api_key = "api_key=" . $key;
+		$this->api_key = $key;
 	
 	}
 	
@@ -16,28 +16,37 @@ class Skycore	{
 		//URL Query Information
 		//---------------------
 		//Static information
-		$action = "action=subscribe"; 
+		//$action = "action=subscribe"; 
 		//Dynamic information
-		$mobile = "mobile=";
-		$campaignid = "campaignid=";
+		//$mobile = "mobile=";
+		//$campaignid = "campaignid=";
 		//Assemble the URL query
-		$api_query = $action . "&" . $this->api_key . "&" . $mobile . $number . "&" . $campaignid . $campid;
+		//$api_query = $action . "&" . $this->api_key . "&" . $mobile . $number . "&" . $campaignid . $campid;
 	
-	
+		$POSTArray['xml']="<REQUEST>
+    <ACTION>subscribe</ACTION>
+    <API_KEY>".$this->api_key."</API_KEY>
+    <CAMPAIGNID>".$campid."</CAMPAIGNID>
+    <MOBILE>".$number."</MOBILE>
+</REQUEST>";
+		
 		//Make the HTTPS request
 		//----------------------
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 
+		/*curl_setopt($ch, CURLOPT_URL, 
 			//Builds the final request URL by concatenating the base URL with the URL query
 			http_build_url('https://scale-secure.skycore.com/API/wxml/1.3/index.php?',
 				array(
 					
 					"query" => $api_query
 				)
-		));
+		));*/
+		//Builds the final request URL by concatenating the base URL with the URL query
+		curl_setopt($ch, CURLOPT_URL, 'https://scale-secure.skycore.com/API/wxml/1.3/index.php?');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $POSTArray);
 		//Set this to 0 if testing on a dev site
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		$result = curl_exec($ch);
